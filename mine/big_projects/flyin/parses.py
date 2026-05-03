daf68a40c8aa94321409e94fdbdbbde010ce4e5b
yasahiru <legendem718@gmail.com>
2026-05-03 19:36:29 +0100

    finish: parse zone is done

--- mine/big_projects/flyin/parses.py
+++ mine/big_projects/flyin/parses.py
@@ -1,25 +1,34 @@
 from typing import Dict, List
+import sys
 
 
 class Parser:
     def __init__(self, file_path):
         self.file_path: str = file_path
         self.nb_drones: int = 0
-        self.zones: List[str] = []
-        self.connections: List[str] = []
+        self.cleared_zones: Dict[str, str] = {}
+        self.cleared_connections: Dict[str, str] = {}
 
     def load(self) -> None:
         try:
-            self._read_file(self.file_path)
-            # self.parse_zones(self.zones)
-        except Exception as e:
+            zones = []
+            connections = []
+            content = self._read_file(self.file_path)
+            for line in content:
+                if line.startswith(("hub", "start_hub", "end_hub")):
+                    zones.append(line)
+                elif line.startswith("connection"):
+                    connections.append(line)
+
+            self.cleared_zones = self.parse_zones(zones)
+        except KeyboardInterrupt as e:
             print(e)
 
     def _read_file(self, filename: str) -> None:
         content = []
         with open(filename, "r") as f:
             for line in f:
-                if line.startswith("#"):
+                if line.startswith("#") or line == "\n":
                     continue
                 else:
                     content.append(line)
@@ -28,20 +37,13 @@ class Parser:
             self.nb_drones = int(content[0].split(":")[1])
         else:
             raise ValueError("nb_drones Should be at the top of the file!!")
+        return content
 
-        for line in content:
-            if line.startswith(("hub", "start_hub", "end_hub")):
-                self.zones.append(line)
-            elif line.startswith("connection"):
-                self.connections.append(line)
 
-    def _parse(self, lines: list[str]) -> Dict[str, str]:
-        ...
-
-    def parse_zones(self) -> Dict[str, List[str]]:
+    def parse_zones(self, zones: List[str]) -> Dict[str, List[str]]:
         filtered_zones = {}
 
-        for z in self.zones:
+        for z in zones:
             if z.startswith("start_hub"):
                 zone_info = (z.split(":")[1]).strip()
                 val = self.clear_zone(zone_info)
@@ -54,66 +56,29 @@ class Parser:
 
     def clear_zone(self, zone: str) -> Dict[str, str]:
         inp = zone.split(" ")
-        # ['base', '0', '0', '[color=green]']
         res = {
             "name": inp[0],
             "x": int(inp[1]),
             "y": int(inp[2]),
-            "meta_data": None
+            "meta_data": {}
         }
 
-        # import sys
-        # sys.exit()
-
         if len(inp) > 3:
-            sep = zone.split("[")
+            s = zone.rfind("[")
+            e = zone.rfind("]") + 1
+            sep = zone[s:e]
+
             if (len(sep) != 1):
-                meta_data = zone.replace(sep[0], "")
-                meta_data.strip()
-                meta_data = meta_data.replace("[", "")
-                meta_data = meta_data.replace("]", "")
-                meta_data = meta_data.split(" ")
+                sep = sep.replace("[", "")
+                sep = sep.replace("]", "")
+                sep.strip()
+                meta_data = sep.split(" ")
 
                 for data in meta_data:
                     d = data.split("=")
                     res["meta_data"].update({d[0]: d[1]})
-
         return res
 
 
 t = Parser("conf.txt")
 t.load()
-_dict = t.parse_zones()
-
-for key, val in _dict.items():
-    print(key, ":")
-    for k, v in val.items():
-        print("\t", end="")
-        print(k, v, sep=" : ")
-    print()
-
-# test = {
-#     "1": {
-#         "a": "b"
-#     }
-# }
-
-# test["1"].update({"c": "d"})
-# print(test)
-
-# v = "base 0 0 [color=green zone=test]"
-
-# print(
-#     v[:v.rfind("[")]
-# )
-
-# print(
-#     t.nb_drones,
-# )
-
-# for _ in t.zones:
-#     print(_, end="")
-# print()
-
-# for _ in t.connections:
-#     print(_, end="")
