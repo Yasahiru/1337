59ed7a6fa7233e367952d179c5ebcaa0611cee0d
hloutman <hloutman@student.1337.ma>
2026-07-28 22:06:33 +0100

    problem solved

--- leetcode/medium/add_two_numbers/add_two_numbers.py
+++ leetcode/medium/add_two_numbers/add_two_numbers.py
@@ -9,56 +9,75 @@ class ListNode:
 
 class Solution:
 
-    def get_numbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Tuple[int, int]:
-        if (l1.next and l2.next):
-            ret = self.get_numbers(l1.next, l2.next)
-            res = (ret[0] * 10 + l1.val, ret[1] * 10 + l2.val)
+    def get_number(self, l1: Optional[ListNode]) -> Tuple[int, int]:
+        if l1.next:
+            ret = self.get_number(l1.next)
+            res = (ret * 10 + l1.val)
             return (res)
-        return (l1.val, l2.val)
+        return (l1.val)
 
-    # converte the number into list[int]
     def num_to_lst(self, number) -> List[int]:
         nbs = []
         while number > 9:
             nbs.append(number % 10)
-            number = int(number / 10)
+            number = (number // 10)
+            print(number)
         nbs.append(number)
         return nbs
 
-    def fill_lkd(self, vals: List[int]):
+    def someTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        num1 = self.get_number(l1)
+        num2 = self.get_number(l2)
+        result = num1 + num2
+        return result
+
+    @staticmethod
+    def fill_lkd(vals: List[int]):
         nodes = []
-        for v in vals:
+        for i, v in enumerate(vals, start=0):
             node = ListNode(v)
             nodes.append(node)
-
         i = 0
         while i < len(nodes) - 1:
             if i < len(nodes):
                 nodes[i].next = nodes[i + 1]
             i += 1
-        return nodes
+        return nodes[0]
 
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        num1, num2 = self.get_numbers(l1, l2)
-        result = num1 + num2
-        return result
+        nums = self.someTwoNumbers(l1, l2)
+        print(nums)
+        lst = self.num_to_lst(nums)
+        lkd = self.fill_lkd(lst)
+        return (lkd)
+
+    def size(self, head: ListNode) -> int:
+        count = 0
+        current = head
+
+        while current is not None:
+            count += 1
+            current = current.next
+
+        return count
 
 
 if __name__ == "__main__":
 
-    node2 = ListNode(3)
-    node1 = ListNode(4, node2)
-    head1 = ListNode(2, node1)
-
-    node4 = ListNode(4)
-    node3 = ListNode(6, node4)
-    head2 = ListNode(5, node3)
-
     s = Solution()
-    nums = s.addTwoNumbers(head1, head2)
-    lst = s.num_to_lst(nums)
-    lkd = s.fill_lkd(lst)
+
+    head1 = s.fill_lkd(
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    )
+
+    head2 = s.fill_lkd([5, 6, 4])
+    res = s.addTwoNumbers(head1, head2)
+    node = res
+    # while node:
+    #     print(node.val, end=" ")
+    #     node = node.next
